5ed00801003c051d70ebce1cfbaab73efd645df6
Sahinur Islam <infosahinur@gmail.com>
2024-01-06 18:09:47 +0600

    [ADD] Dashboard Home

--- src/Pages/Dashboard/DashboardHome/DashboardHome.jsx
+++ src/Pages/Dashboard/DashboardHome/DashboardHome.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React from "react";
 
 function DashboardHome() {
   return (
-    <div>DashboardHome</div>
-  )
+    <div>
+      <div className="flex">
+        <div className="w-[708px] flex flex-col pl-[20px] pt-[20px]">
+          <div className="flex">
+            <div className="">
+              <img src="" alt="" />
+              <div>
+                <h1>Total User</h1>
+                <p>780</p>
+              </div>
+            </div>
+            <div className="">2</div>
+            <div className="">3</div>
+          </div>
+          <div className="">2</div>
+          <div className="">3</div>
+        </div>
+        <div className="w-[342px] pl-[20px] pt-[20px]">right</div>
+      </div>
+    </div>
+  );
 }
 
-export default DashboardHome
+export default DashboardHome;
