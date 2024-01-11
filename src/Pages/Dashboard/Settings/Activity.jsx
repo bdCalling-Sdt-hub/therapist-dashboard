67bcbbdf8297d989a737318ce977f8000f2a63a8
Sahinur Islam <infosahinur@gmail.com>
2024-01-11 16:38:17 +0600

    [ADD] Activity

--- src/Pages/Dashboard/Settings/Activity.jsx
+++ src/Pages/Dashboard/Settings/Activity.jsx
@@ -1,9 +1,180 @@
-import React from 'react'
+import React from "react";
 
 function Activity() {
   return (
-    <div>Activity</div>
-  )
+    <div>
+      <div className="mb-[24px]">
+        <h1 className="text-[24px] font-semibold ">Your Login Activity</h1>
+        <p>
+          You’re signed in on these devices or have been in the last 30 days.
+        </p>
+      </div>
+      <div className="rounded-xl border-1 h-[69vh] overflow-hidden overflow-y-scroll w-full  p-[24px] flex flex-col bg-white border-secondary">
+        <div className="flex justify-between items-center px-[24px] py-[8px] border-b-[1px] pb-3 border-secondary">
+          <div>
+            <img
+              className="w-[80px] h-[80px]"
+              src="https://i.ibb.co/pxFgvjs/Laptop.png"
+              alt=""
+            />
+          </div>
+          <div>
+            <h2 className="text-[18px]">Windows</h2>
+            <p>IP: 192.168.10.01</p>
+            <p>Google Chrome</p>
+            <p className="text-primary">Current Session</p>
+          </div>
+          <div>
+            <button className="text-[#F44336] text-[14px] hover:bg-[#F44336] hover:text-white rounded font-semibold border-[1px] px-[25px] py-[5px] border-[#F44336]">Sign Out</button>
+          </div>
+        </div>
+        <div className="flex justify-between items-center px-[24px] py-[8px] border-b-[1px] pb-3 border-secondary">
+          <div>
+            <img
+              className="w-[80px] h-[80px]"
+              src="https://i.ibb.co/pxFgvjs/Laptop.png"
+              alt=""
+            />
+          </div>
+          <div>
+            <h2 className="text-[18px]">Windows</h2>
+            <p>IP: 192.168.10.01</p>
+            <p>Google Chrome</p>
+            <p className="">Current Session</p>
+          </div>
+          <div>
+            <button className="text-[#F44336] hover: text-[14px]  hover:bg-[#F44336] hover:text-white rounded font-semibold border-[1px] px-[25px] py-[5px] border-[#F44336]">Sign Out</button>
+          </div>
+        </div>
+        <div className="flex justify-between items-center px-[24px] py-[8px] border-b-[1px] pb-3 border-secondary">
+          <div>
+            <img
+              className="w-[80px] h-[80px]"
+              src="https://i.ibb.co/pxFgvjs/Laptop.png"
+              alt=""
+            />
+          </div>
+          <div>
+            <h2 className="text-[18px]">Windows</h2>
+            <p>IP: 192.168.10.01</p>
+            <p>Google Chrome</p>
+            <p className="">Current Session</p>
+          </div>
+          <div>
+            <button className="text-[#F44336] hover: text-[14px]  hover:bg-[#F44336] hover:text-white rounded font-semibold border-[1px] px-[25px] py-[5px] border-[#F44336]">Sign Out</button>
+          </div>
+        </div>
+        <div className="flex justify-between items-center px-[24px] py-[8px] border-b-[1px] pb-3 border-secondary">
+          <div>
+            <img
+              className="w-[80px] h-[80px]"
+              src="https://i.ibb.co/pxFgvjs/Laptop.png"
+              alt=""
+            />
+          </div>
+          <div>
+            <h2 className="text-[18px]">Windows</h2>
+            <p>IP: 192.168.10.01</p>
+            <p>Google Chrome</p>
+            <p className="">Current Session</p>
+          </div>
+          <div>
+            <button className="text-[#F44336] hover: text-[14px]  hover:bg-[#F44336] hover:text-white rounded font-semibold border-[1px] px-[25px] py-[5px] border-[#F44336]">Sign Out</button>
+          </div>
+        </div>
+        <div className="flex justify-between items-center px-[24px] py-[8px] border-b-[1px] pb-3 border-secondary">
+          <div>
+            <img
+              className="w-[80px] h-[80px]"
+              src="https://i.ibb.co/pxFgvjs/Laptop.png"
+              alt=""
+            />
+          </div>
+          <div>
+            <h2 className="text-[18px]">Windows</h2>
+            <p>IP: 192.168.10.01</p>
+            <p>Google Chrome</p>
+            <p className="">Current Session</p>
+          </div>
+          <div>
+            <button className="text-[#F44336] hover: text-[14px]  hover:bg-[#F44336] hover:text-white rounded font-semibold border-[1px] px-[25px] py-[5px] border-[#F44336]">Sign Out</button>
+          </div>
+        </div>
+        <div className="flex justify-between items-center px-[24px] py-[8px] border-b-[1px] pb-3 border-secondary">
+          <div>
+            <img
+              className="w-[80px] h-[80px]"
+              src="https://i.ibb.co/pxFgvjs/Laptop.png"
+              alt=""
+            />
+          </div>
+          <div>
+            <h2 className="text-[18px]">Windows</h2>
+            <p>IP: 192.168.10.01</p>
+            <p>Google Chrome</p>
+            <p className="">Current Session</p>
+          </div>
+          <div>
+            <button className="text-[#F44336] hover: text-[14px]  hover:bg-[#F44336] hover:text-white rounded font-semibold border-[1px] px-[25px] py-[5px] border-[#F44336]">Sign Out</button>
+          </div>
+        </div>
+        <div className="flex justify-between items-center px-[24px] py-[8px] border-b-[1px] pb-3 border-secondary">
+          <div>
+            <img
+              className="w-[80px] h-[80px]"
+              src="https://i.ibb.co/pxFgvjs/Laptop.png"
+              alt=""
+            />
+          </div>
+          <div>
+            <h2 className="text-[18px]">Windows</h2>
+            <p>IP: 192.168.10.01</p>
+            <p>Google Chrome</p>
+            <p className="">Current Session</p>
+          </div>
+          <div>
+            <button className="text-[#F44336] hover: text-[14px]  hover:bg-[#F44336] hover:text-white rounded font-semibold border-[1px] px-[25px] py-[5px] border-[#F44336]">Sign Out</button>
+          </div>
+        </div>
+        <div className="flex justify-between items-center px-[24px] py-[8px] border-b-[1px] pb-3 border-secondary">
+          <div>
+            <img
+              className="w-[80px] h-[80px]"
+              src="https://i.ibb.co/pxFgvjs/Laptop.png"
+              alt=""
+            />
+          </div>
+          <div>
+            <h2 className="text-[18px]">Windows</h2>
+            <p>IP: 192.168.10.01</p>
+            <p>Google Chrome</p>
+            <p className="">Current Session</p>
+          </div>
+          <div>
+            <button className="text-[#F44336] hover: text-[14px]  hover:bg-[#F44336] hover:text-white rounded font-semibold border-[1px] px-[25px] py-[5px] border-[#F44336]">Sign Out</button>
+          </div>
+        </div>
+        <div className="flex justify-between items-center px-[24px] py-[8px] border-b-[1px] pb-3 border-secondary">
+          <div>
+            <img
+              className="w-[80px] h-[80px]"
+              src="https://i.ibb.co/pxFgvjs/Laptop.png"
+              alt=""
+            />
+          </div>
+          <div>
+            <h2 className="text-[18px]">Windows</h2>
+            <p>IP: 192.168.10.01</p>
+            <p>Google Chrome</p>
+            <p className="">Current Session</p>
+          </div>
+          <div>
+            <button className="text-[#F44336] hover: text-[14px]  hover:bg-[#F44336] hover:text-white rounded font-semibold border-[1px] px-[25px] py-[5px] border-[#F44336]">Sign Out</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Activity
+export default Activity;
