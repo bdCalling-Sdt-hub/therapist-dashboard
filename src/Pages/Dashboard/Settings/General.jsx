615e73af97c693dc71bb1eeb3168f0c8b139c64a
Sahinur Islam <infosahinur@gmail.com>
2024-01-11 11:24:47 +0600

    [ADD] Website Basic Details

--- src/Pages/Dashboard/Settings/General.jsx
+++ src/Pages/Dashboard/Settings/General.jsx
@@ -1,9 +1,134 @@
-import React from 'react'
+import React, { useState } from "react";
+import { PlusOutlined } from "@ant-design/icons";
+import { Modal, Input, Upload } from "antd";
+const getBase64 = (file) =>
+  new Promise((resolve, reject) => {
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onload = () => resolve(reader.result);
+    reader.onerror = (error) => reject(error);
+  });
 
 function General() {
+  const [previewOpen, setPreviewOpen] = useState(false);
+  const [previewImage, setPreviewImage] = useState("");
+  const [previewTitle, setPreviewTitle] = useState("");
+  const [fileList, setFileList] = useState([
+    {
+      uid: "-1",
+      name: "image.png",
+      status: "done",
+      url: "https://i.ibb.co/BZyk1GG/image-2.png",
+    },
+
+    // {
+    //   uid: '-5',
+    //   name: 'image.png',
+    //   status: 'error',
+    // },
+  ]);
+  const handleCancel = () => setPreviewOpen(false);
+  const handlePreview = async (file) => {
+    if (!file.url && !file.preview) {
+      file.preview = await getBase64(file.originFileObj);
+    }
+    setPreviewImage(file.url || file.preview);
+    setPreviewOpen(true);
+    setPreviewTitle(
+      file.name || file.url.substring(file.url.lastIndexOf("/") + 1)
+    );
+  };
+  const handleChange = ({ fileList: newFileList }) => setFileList(newFileList);
+
+  const uploadButton = (
+    <button
+      style={{
+        border: 0,
+        background: "none",
+      }}
+      type="button"
+    >
+      <PlusOutlined />
+      <div
+        style={{
+          marginTop: 8,
+        }}
+      >
+        Upload
+      </div>
+    </button>
+  );
   return (
-    <div>General</div>
-  )
+    <div>
+      <div>
+        <h1 className="text-[24px] font-semibold mb-[24px]">
+          Website Basic Details
+        </h1>
+      </div>
+      <div className="rounded-xl border-1 h-[325px] p-[24px] bg-white border-secondary">
+        <div className="flex flex-col justify-between">
+          <p className="text-[12px] ml-1 mb-2 text-primary">Website Link</p>
+          <Input
+            className="w-full h-[40px] border-1 border-secondary rounded-xl p-[10px] text-[16px] font-medium"
+            placeholder="http://mindgaze.com"
+            suffix={
+              <svg
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M8 7V15C8 16.1046 8.89543 17 10 17H16M8 7V5C8 3.89543 8.89543 3 10 3H14.5858C14.851 3 15.1054 3.10536 15.2929 3.29289L19.7071 7.70711C19.8946 7.89464 20 8.149 20 8.41421V15C20 16.1046 19.1046 17 18 17H16M8 7H6C4.89543 7 4 7.89543 4 9V19C4 20.1046 4.89543 21 6 21H14C15.1046 21 16 20.1046 16 19V17"
+                  stroke="#B9B9B9"
+                  stroke-width="2"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                />
+              </svg>
+            }
+          />
+        </div>
+        <div>
+          <div>
+            <h1 className="text-[18px] my-[16px]  font-semibold">Logo</h1>
+          </div>
+          <div>
+            <Upload
+              action="https://run.mocky.io/v3/435e224c-44fb-4773-9faf-380c5e6a2188"
+              listType="picture-card"
+              fileList={fileList}
+              onPreview={handlePreview}
+              onChange={handleChange}
+            >
+              {fileList.length >= 8 ? null : uploadButton}
+            </Upload>
+            <Modal
+              open={previewOpen}
+              title={previewTitle}
+              footer={null}
+              onCancel={handleCancel}
+            >
+              <img
+                alt="example"
+                style={{
+                  width: "100%",
+                }}
+                src={previewImage}
+              />
+            </Modal>
+          </div>
+          <div>
+            <p className="mt-[12px]">
+              Recommend image size is{" "}
+              <span className="font-medium">168x168px</span>
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default General
+export default General;
