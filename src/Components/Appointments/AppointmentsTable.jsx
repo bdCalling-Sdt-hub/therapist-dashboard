a25b58586e408bd855488902dae5db9af4efff88
Sahinur Islam <infosahinur@gmail.com>
2024-01-17 12:47:38 +0600

    [ADD] All Appointments modal

--- src/Components/Appointments/AppointmentsTable.jsx
+++ src/Components/Appointments/AppointmentsTable.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Table, Pagination, ConfigProvider, Modal } from "antd";
+import { Table, Pagination, ConfigProvider, Modal, Rate } from "antd";
 
 function AppointmentsTable() {
   const [isModalOpen, setIsModalOpen] = useState(false);
@@ -171,7 +171,10 @@ function AppointmentsTable() {
       render: (_, record) => (
         <>
           <div>
-            <p onClick={handleOpenModal} className="px-6 py-1 text-[14px] text-primary hover:bg-primary hover:text-white  font-medium cursor-pointer border-[1px] border-primary rounded inline-block">
+            <p
+              onClick={handleOpenModal}
+              className="px-6 py-1 text-[14px] text-primary hover:bg-primary hover:text-white  font-medium cursor-pointer border-[1px] border-primary rounded inline-block"
+            >
               Details
             </p>
           </div>
@@ -211,47 +214,99 @@ function AppointmentsTable() {
       </div>
 
       <Modal
-          visible={isModalOpen}
-          title={
-            <div className=" py-2 border-b-[1px] border-primary font-['Montserrat'] ">
-              <span className="text-[24px] text-primary ">Patient Details</span>
-              <p className=" text-[14px] text-[#B9B9B9]">
-                See all details about John Doe
-              </p>
+        visible={isModalOpen}
+        title={
+          <div className=" py-2 border-b-[1px] border-primary font-['Montserrat'] ">
+            <div className="flex items-center gap-5">
+              <span className="text-[24px] text-primary ">
+                Appointment ID: #00014563{" "}
+              </span>
+              <span className="text-primary text-[12px] rounded inline-block px-1 bg-secondary">
+                Completed
+              </span>
             </div>
-          }
-          onCancel={handleCancel}
-          centered
-          footer={[]}
-          width={500}
-        >
-          <div className="flex flex-col">
-            <div className="flex items-center gap-5 border-b-[1px] pb-2 border-primary">
-              <img
-                className="w-[70px] h-[70px]"
-                src="https://i.ibb.co/Pw9b56k/b3b76175bc84084ec18597109498f96d.png"
-                alt=""
-              />
-              <h1 className="text-primary text-[24px] ">John Doe</h1>
-            </div>
-            <div>
-              <h1 className=" text-[24px] font-semibold">Information</h1>
-              <div className="text-[16px] flex flex-col gap-2">
-                <p>Name: John Doe</p>
-                <p>Email: 6bHnN@example.com</p>
-                <p>Phone Number: 1234567890</p>
-                <p>Date of Birth: 01/01/2000</p>
-                <p>Gender: Male</p>
-                <p>Survey: Teen Therapy(13-18)</p>
+            <p className=" text-[14px] text-[#B9B9B9]">
+              See all details about John Doe
+            </p>
+          </div>
+        }
+        onCancel={handleCancel}
+        centered
+        footer={[]}
+        width={900}
+      >
+        <div className="flex flex-col">
+          <div className="flex items-center gap-5 border-b-[1px] pb-2 border-primary">
+            <div className="flex flex-col gap-5">
+              <div>
+                <h1 className="text-[24px] font-semibold">Patient Details</h1>
+                <div className="p-[8px]">
+                  <p className="text-[16px]">Name: John Doe</p>
+                  <p className="text-[16px]">Email: johndoe@gmail.com</p>
+                </div>
+              </div>
+              <div>
+                <h1 className="text-[24px] font-semibold">
+                  Therapists Details
+                </h1>
+                <div className="p-[8px]">
+                  <p className="text-[16px]">Name: John Doe</p>
+                  <p className="text-[16px]">Email: johndoe@gmail.com</p>
+                </div>
               </div>
             </div>
-            <div className="flex mt-[24px]">
-              <p className="bg-primary cursor-pointer px-5 py-2 text-white rounded">
-                Block
+          </div>
+          <div className="flex justify-between gap-5">
+            <div className="w-[720px] border-r-[1px] border-primary pl-5">
+              <h1 className=" text-[24px] text-primary mt-[16px] font-semibold">
+                Appointment Details
+              </h1>
+              <div>
+                <p className="text-[16px]">
+                  <span className="font-semibold">Appointment Date: </span>
+                  12/12/2023
+                </p>
+                <p className="text-[16px]">
+                  <span className="font-semibold">Appointment Time: </span>
+                  02:00pm - 02:30pm
+                </p>
+                <p className="text-[16px]">
+                  <span className="font-semibold">Treatment: </span>
+                  Teen Therapy(13-18)
+                </p>
+                <p className="text-[16px]">
+                  <span className="font-semibold">Total Session Time: </span>
+                  01:30:36
+                </p>
+                <p className="text-[16px]">
+                  <span className="font-semibold">Session Type: </span>
+                  Video Call
+                </p>
+              </div>
+            </div>
+            <div>
+              <h1 className=" text-[24px] mt-[16px] text-primary font-semibold">
+                Patient Review
+              </h1>
+              <div className="py-4">
+                <Rate allowHalf defaultValue={5} disabled />
+              </div>
+              <p className="text-[16px]">
+                Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                laboris nisi ut aliquip ex ea commodo con
               </p>
             </div>
           </div>
-        </Modal>0
+          <div className="flex gap-4 mt-[24px] mx-auto">
+            <p className="bg-primary cursor-pointer px-5 py-2 text-white rounded">
+              Block
+            </p>
+            <p className="bg-primary cursor-pointer px-5 py-2 text-white rounded">
+              Download as PDF
+            </p>
+          </div>
+        </div>
+      </Modal>
     </div>
   );
 }
