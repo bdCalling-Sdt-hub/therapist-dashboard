6a93ea631e179f492ce9cf84999715496fa399cf
Sahinur Islam <infosahinur@gamil.com>
2024-01-21 10:44:57 +0600

    [ADD] Patient Request

--- src/Pages/Dashboard/Patients/PatientsRequest.jsx
+++ src/Pages/Dashboard/Patients/PatientsRequest.jsx
@@ -2,14 +2,155 @@ import React from "react";
 
 function PatientsRequest() {
   return (
-    <div>
+    <div className="p-[24px] flex ">
       <div>
-        <div>
-          
+        <div className=" w-[790px]">
+          <h1 className="text-[24px] font-semibold text-white rounded bg-primary p-[16px]">
+            Patient Request
+          </h1>
         </div>
-        <div></div>
+        <div className="grid grid-cols-2 gap-5">
+          <div className="flex bg-white rounded-md w-[385px] gap-5 mt-5 p-[16px]">
+            <div>
+              <img
+                className="w-[100px] h-[100px] rounded-full"
+                src="https://i.ibb.co/f1YyLM4/Ellipse-2322.png"
+                alt=""
+              />
+            </div>
+            <div className="flex flex-col gap-5">
+              <div>
+                <h1 className="text-[22px] font-semibold">Dianne Russell</h1>
+                <p className="text-[14px] font-medium ">Teen Therapy(13-18)</p>
+              </div>
+              <div className="flex gap-5">
+                <button className="text-white bg-primary text-[18px] rounded p-[7px] px-[20px]">
+                  Accept
+                </button>
+                <button className=" border-[1px] border-primary text-primary text-[18px]  rounded  p-[7px] px-[20px] ">
+                  Details
+                </button>
+              </div>
+            </div>
+          </div>
+          <div className="flex bg-white rounded-md w-[385px] gap-5 mt-5 p-[16px]">
+            <div>
+              <img
+                className="w-[100px] h-[100px] rounded-full"
+                src="https://i.ibb.co/f1YyLM4/Ellipse-2322.png"
+                alt=""
+              />
+            </div>
+            <div className="flex flex-col gap-5">
+              <div>
+                <h1 className="text-[22px] font-semibold">Dianne Russell</h1>
+                <p className="text-[14px] font-medium ">Teen Therapy(13-18)</p>
+              </div>
+              <div className="flex gap-5">
+                <button className="text-white bg-primary text-[18px] rounded p-[7px] px-[20px]">
+                  Accept
+                </button>
+                <button className=" border-[1px] border-primary text-primary text-[18px]  rounded  p-[7px] px-[20px] ">
+                  Details
+                </button>
+              </div>
+            </div>
+          </div>
+          <div className="flex bg-white rounded-md w-[385px] gap-5 mt-5 p-[16px]">
+            <div>
+              <img
+                className="w-[100px] h-[100px] rounded-full"
+                src="https://i.ibb.co/f1YyLM4/Ellipse-2322.png"
+                alt=""
+              />
+            </div>
+            <div className="flex flex-col gap-5">
+              <div>
+                <h1 className="text-[22px] font-semibold">Dianne Russell</h1>
+                <p className="text-[14px] font-medium ">Teen Therapy(13-18)</p>
+              </div>
+              <div className="flex gap-5">
+                <button className="text-white bg-primary text-[18px] rounded p-[7px] px-[20px]">
+                  Accept
+                </button>
+                <button className=" border-[1px] border-primary text-primary text-[18px]  rounded  p-[7px] px-[20px] ">
+                  Details
+                </button>
+              </div>
+            </div>
+          </div>
+          <div className="flex bg-white rounded-md w-[385px] gap-5 mt-5 p-[16px]">
+            <div>
+              <img
+                className="w-[100px] h-[100px] rounded-full"
+                src="https://i.ibb.co/f1YyLM4/Ellipse-2322.png"
+                alt=""
+              />
+            </div>
+            <div className="flex flex-col gap-5">
+              <div>
+                <h1 className="text-[22px] font-semibold">Dianne Russell</h1>
+                <p className="text-[14px] font-medium ">Teen Therapy(13-18)</p>
+              </div>
+              <div className="flex gap-5">
+                <button className="text-white bg-primary text-[18px] rounded p-[7px] px-[20px]">
+                  Accept
+                </button>
+                <button className=" border-[1px] border-primary text-primary text-[18px]  rounded  p-[7px] px-[20px] ">
+                  Details
+                </button>
+              </div>
+            </div>
+          </div>
+          <div className="flex bg-white rounded-md w-[385px] gap-5 mt-5 p-[16px]">
+            <div>
+              <img
+                className="w-[100px] h-[100px] rounded-full"
+                src="https://i.ibb.co/f1YyLM4/Ellipse-2322.png"
+                alt=""
+              />
+            </div>
+            <div className="flex flex-col gap-5">
+              <div>
+                <h1 className="text-[22px] font-semibold">Dianne Russell</h1>
+                <p className="text-[14px] font-medium ">Teen Therapy(13-18)</p>
+              </div>
+              <div className="flex gap-5">
+                <button className="text-white bg-primary text-[18px] rounded p-[7px] px-[20px]">
+                  Accept
+                </button>
+                <button className=" border-[1px] border-primary text-primary text-[18px]  rounded  p-[7px] px-[20px] ">
+                  Details
+                </button>
+              </div>
+            </div>
+          </div>
+          <div className="flex bg-white rounded-md w-[385px] gap-5 mt-5 p-[16px]">
+            <div>
+              <img
+                className="w-[100px] h-[100px] rounded-full"
+                src="https://i.ibb.co/f1YyLM4/Ellipse-2322.png"
+                alt=""
+              />
+            </div>
+            <div className="flex flex-col gap-5">
+              <div>
+                <h1 className="text-[22px] font-semibold">Dianne Russell</h1>
+                <p className="text-[14px] font-medium ">Teen Therapy(13-18)</p>
+              </div>
+              <div className="flex gap-5">
+                <button className="text-white bg-primary text-[18px] rounded p-[7px] px-[20px]">
+                  Accept
+                </button>
+                <button className=" border-[1px] border-primary text-primary text-[18px]  rounded  p-[7px] px-[20px] ">
+                  Details
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
-      <div></div>
+      <div className="w-[260px]"></div>
     </div>
   );
 }
