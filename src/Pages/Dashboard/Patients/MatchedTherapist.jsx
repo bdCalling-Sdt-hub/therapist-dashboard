cab807a60488530ef0a7ff6d6738b47308f9fd6e
Sahinur Islam <infosahinur@gmail.com>
2024-01-25 11:18:47 +0600

    [ADD] Matched Request

--- src/Pages/Dashboard/Patients/MatchedTherapist.jsx
+++ src/Pages/Dashboard/Patients/MatchedTherapist.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import Status from "../../../Components/Patients/MatchedTherapist/Status";
+import MatchedTherapistList from "../../../Components/Patients/MatchedTherapist/MatchedTherapistList";
 
 function MatchedTherapist() {
   return (
     <div className="p-[32px]">
       <Status />
-      {/* <AllAppointments /> */}
+      <MatchedTherapistList />
     </div>
   );
 }
