6430850452e1c0426c5c24fb345bf0610afe15e3
Sahinur Islam <infosahinur@gmail.com>
2024-01-31 10:45:29 +0600

    [ADD] Manage Questionnaires

--- src/Pages/Dashboard/Appointments/Appointments.jsx
+++ src/Pages/Dashboard/Appointments/Appointments.jsx
@@ -3,6 +3,8 @@ import Status from "../../../Components/Appointments/Status";
 import AllAppointments from "../../../Components/Appointments/AllAppointments";
 
 function Appointments() {
+
+  
   return (
     <div className="p-[32px]">
       <Status />
--- src/Pages/Questionnaires/ManageQuestionnaires.jsx
+++ src/Pages/Questionnaires/ManageQuestionnaires.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { MinusCircleOutlined, PlusOutlined } from "@ant-design/icons";
-import { Button, Form, Input, Checkbox } from "antd";
+import { Button, Form, Input, Select, Checkbox } from "antd";
 import { useNavigate } from "react-router-dom";
 
 const formItemLayout = {
@@ -39,6 +39,7 @@ function ManageQuestionnaires() {
   const navigate = useNavigate();
   const [questionnairesName, setQuestionnairesName] =
     useState("Individual Therapy");
+  const [fullSurvey, setFullSurvey] = useState();
 
   const makeQuestion = [
     {
@@ -188,6 +189,15 @@ function ManageQuestionnaires() {
     console.log("Received values of form:", values);
   };
 
+  const [questionType, setQuestionType] = useState("Paragraph");
+  const [question, setQuestion] = useState("");
+  const [option, setOption] = useState([]);
+
+  const handleChange = (value) => {
+    console.log(value);
+    setQuestionType(value);
+  };
+
   return (
     <div>
       <div className="p-[24px]">
@@ -207,103 +217,193 @@ function ManageQuestionnaires() {
           />
         </div>
         <div>
-          <Form
-            name="dynamic_form_item"
-            {...formItemLayoutWithOutLabel}
-            onFinish={onFinish}
-            style={{
-              maxWidth: 600,
-            }}
-          >
-            <Form.List
-              name="names"
-              rules={[
-                {
-                  validator: async (_, names) => {
-                    if (!names || names.length < 2) {
-                      return Promise.reject(new Error("At least 2 passengers"));
-                    }
-                  },
-                },
-              ]}
-            >
-              {(fields, { add, remove }, { errors }) => (
-                <>
-                  {fields.map((field, index) => (
-                    <Form.Item
-                      {...(index === 0
-                        ? formItemLayout
-                        : formItemLayoutWithOutLabel)}
-                      label={index === 0 ? "Passengers" : ""}
-                      required={false}
-                      key={field.key}
-                    >
-                      <Form.Item
-                        {...field}
-                        validateTrigger={["onChange", "onBlur"]}
-                        rules={[
-                          {
-                            required: true,
-                            whitespace: true,
-                            message:
-                              "Please input passenger's name or delete this field.",
-                          },
-                        ]}
-                        noStyle
-                      ></Form.Item>
-                      <Form.Item
-                        {...field}
-                        name={[field.name, "isChecked"]}
-                        valuePropName="checked"
-                        noStyle
+          <div className="flex bg-white  rounded-xl  mt-[24px]">
+            <div className="w-[790px]">
+              <Form
+                className="py-[24px]"
+                name="dynamic_form_item"
+                {...formItemLayoutWithOutLabel}
+                onFinish={onFinish}
+                style={{
+                  maxWidth: 600,
+                }}
+              >
+                <Form.List
+                  name="questions"
+                  initialValue={[
+                    {
+                      question: "",
+                      options: [""],
+                      isChecked: false,
+                    },
+                  ]}
+                >
+                  {(fields, { add, remove }) => (
+                    <>
+                      {fields.map((field, index) => (
+                        <Form.Item
+                          {...(index === 0
+                            ? formItemLayout
+                            : formItemLayoutWithOutLabel)}
+                          label={index === 0 ? "Questions" : ""}
+                          required={false}
+                          key={field.key}
+                        >
+                          <Form.Item
+                            {...field}
+                            name={[field.name, "question"]}
+                            validateTrigger={["onChange", "onBlur"]}
+                            rules={[
+                              {
+                                required: true,
+                                whitespace: true,
+                                message: "Please input the question.",
+                              },
+                            ]}
+                            noStyle
+                          >
+                            <Input
+                              className="mb-5"
+                              placeholder="Enter Question"
+                            />
+                          </Form.Item>
+                          <Form.Item
+                            {...field}
+                            name={[field.name, "options"]}
+                            validateTrigger={["onChange", "onBlur"]}
+                            rules={[
+                              {
+                                required: true,
+                                message: "Please input at least one option.",
+                              },
+                            ]}
+                            noStyle
+                          >
+                            <Form.List name={[field.name, "options"]}>
+                              {(
+                                optionFields,
+                                { add: addOption, remove: removeOption }
+                              ) => (
+                                <div className="flex flex-col gap-5">
+                                  {optionFields.map(
+                                    (optionField, optionIndex) => (
+                                      <div
+                                        className="flex gap-5"
+                                        key={optionField.key}
+                                      >
+                                        <Form.Item
+                                          {...optionField}
+                                          validateTrigger={[
+                                            "onChange",
+                                            "onBlur",
+                                          ]}
+                                          rules={[
+                                            {
+                                              required: true,
+                                              whitespace: true,
+                                              message: `Please input the option ${
+                                                optionIndex + 1
+                                              }.`,
+                                            },
+                                          ]}
+                                          noStyle
+                                        >
+                                          <Input
+                                            placeholder={`Option ${
+                                              optionIndex + 1
+                                            }`}
+                                          />
+                                        </Form.Item>
+                                        {optionFields.length > 1 && (
+                                          <MinusCircleOutlined
+                                            className="dynamic-delete-button"
+                                            onClick={() =>
+                                              removeOption(optionField.name)
+                                            }
+                                          />
+                                        )}
+                                      </div>
+                                    )
+                                  )}
+                                  <Button
+                                    type="dashed"
+                                    onClick={() => addOption()}
+                                    icon={<PlusOutlined />}
+                                  >
+                                    Add Option
+                                  </Button>
+                                </div>
+                              )}
+                            </Form.List>
+                          </Form.Item>
+                          {/* <Form.Item
+                          {...field}
+                          name={[field.name, "isChecked"]}
+                          valuePropName="checked"
+                          noStyle
+                        >
+                          <Checkbox style={{ marginLeft: "8px" }}>
+                            Is Checked
+                          </Checkbox>
+                        </Form.Item> */}
+                          {fields.length > 1 && (
+                            <MinusCircleOutlined
+                              className="dynamic-delete-button"
+                              onClick={() => remove(field.name)}
+                            />
+                          )}
+                        </Form.Item>
+                      ))}
+                      {/* <Form.Item>
+                      <Button
+                        type="dashed"
+                        onClick={() => add()}
+                        style={{
+                          width: "60%",
+                        }}
+                        icon={<PlusOutlined />}
                       >
-                        <Checkbox style={{ marginLeft: "8px" }}>
-                          Is Checked
-                        </Checkbox>
-                      </Form.Item>
-                      {fields.length > 1 ? (
-                        <MinusCircleOutlined
-                          className="dynamic-delete-button"
-                          onClick={() => remove(field.name)}
-                        />
-                      ) : null}
-                    </Form.Item>
-                  ))}
-                  <Form.Item>
-                    <Button
-                      type="dashed"
-                      onClick={() => add()}
-                      style={{
-                        width: "60%",
-                      }}
-                      icon={<PlusOutlined />}
-                    >
-                      Add field
-                    </Button>
-                    <Button
-                      type="dashed"
-                      onClick={() => {
-                        add("The head item", 0);
-                      }}
-                      style={{
-                        width: "60%",
-                        marginTop: "20px",
-                      }}
-                      icon={<PlusOutlined />}
-                    >
-                      Add field at head
-                    </Button>
-                    <Form.ErrorList errors={errors} />
-                  </Form.Item>
-                </>
-              )}
-            </Form.List>
-            <Form.Item>
-              <Button type="primary" htmlType="submit">
-                Submit
-              </Button>
-            </Form.Item>
-          </Form>
+                        Add Question
+                      </Button>
+                    </Form.Item> */}
+                    </>
+                  )}
+                </Form.List>
+                <Form.Item>
+                  <Button htmlType="submit">Submit</Button>
+                </Form.Item>
+              </Form>
+            </div>
+            <div className="p-[24px] border-l-2 border-secondary">
+              <Select
+                className=""
+                labelInValue
+                defaultValue={{
+                  value: questionType,
+                  label: questionType,
+                }}
+                style={{
+                  width: 250,
+                  height: 40,
+                }}
+                onChange={handleChange}
+                options={[
+                  {
+                    value: "Paragraph",
+                    label: "Paragraph",
+                  },
+                  {
+                    value: "Multiple Choice",
+                    label: "Multiple Choice",
+                  },
+                  {
+                    value: "Checkbox",
+                    label: "Checkbox",
+                  },
+                ]}
+              />
+            </div>
+          </div>
         </div>
       </div>
     </div>
